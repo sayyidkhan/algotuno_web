bcedf8ecbe7ddeb567d28df2ae158d6b80c1c476
svenandten <rickezxc@gmail.com>
2022-05-02 21:47:52 +0800

    amended HSP api

--- pages/api/stock/get_hsp.ts
+++ pages/api/stock/get_hsp.ts
@@ -24,8 +24,9 @@ export default async (req, res) => {
             return
         }
 
-        var ticker_symbol;
-
+        var ticker_symbol, stock_id;
+        var hsp_start_date, hsp_end_date;
+        var sort = "asc";
 
         try{
 
@@ -38,17 +39,109 @@ export default async (req, res) => {
                 }
             });
 
-            // return the corresponding stockID
-            const stock_id = stock_record.stockID;
+            if (stock_record.length != 0) {
+                // return the corresponding stockID
+                stock_id = stock_record.stockID;
+            } else {
+                console.log(`Stock ${ticker_symbol} does not exist`)
+                return res.status(406).json({
+                    "message" : `Stock ${ticker_symbol} does not exist`
+                });
+            }
 
-            // return the HSP for the stock
-            const all_records = await prisma.historical_Stock_Price.findMany({
-                where:{
-                    stockID : stock_id
+            hsp_start_date = new Date(req.body.start_date);
+            hsp_end_date   = new Date(req.body.end_date);
+
+            // // check if HSP start and end dates are specified
+            // if(req.body.start_date && req.body.end_date){
+
+            //     if (isNaN(hsp_start_date)) {
+            //         console.log("Invalid start date specified")
+            //         return res.status(406).json({
+            //             "message" : "Invalid start date specified"
+            //         });
+            //     } else if (isNaN(hsp_end_date)) {
+            //         console.log("Invalid end date specified")
+            //         return res.status(406).json({
+            //             "message" : "Invalid end date specified"
+            //         });
+            //     }
+            // } else {
+            //     console.log("No specified dates");
+            // }
+
+            // check if sort type (asc/desc) is specified
+            if(req.body.sort) {
+                sort = req.body.sort.toLowerCase();
+                if(sort == "desc" || sort == "asc"){
+                    //pass
+                } else {
+                    sort = "asc";
+                    console.log("Invalid sort type specified, results will be sorted based on ASC order");
                 }
-            })
+            } else {
+                //pass 
+            }
 
-            const successMsg = `Found ${all_records.length} records for ${ticker_symbol}`;
+            var successMsg, all_records;
+
+            // both start and end dates invalid / not specified
+            if(isNaN(hsp_start_date) && isNaN(hsp_end_date)){
+                console.log("Start and end dates invalid");
+                all_records = await prisma.historical_Stock_Price.findMany({
+                    where:{
+                        stockID : stock_id
+                    },
+                    orderBy: {
+                        Date : sort
+                    }
+                });
+            } else if (isNaN(hsp_end_date)){
+                // only start date is valid; get all records newer than start date
+                console.log("End dates invalid");
+                all_records = await prisma.historical_Stock_Price.findMany({
+                    where:{
+                        stockID : stock_id,
+                        Date : {
+                            gte: hsp_start_date
+                        }
+                    },
+                    orderBy: {
+                        Date : sort
+                    }
+                });
+            } else if (isNaN(hsp_start_date)){
+                // only end date is valid
+                console.log("Start dates invalid");
+                all_records = await prisma.historical_Stock_Price.findMany({
+                    where:{
+                        stockID : stock_id,
+                        Date : {
+                            lte: hsp_end_date
+                        }
+                    },
+                    orderBy: {
+                        Date : sort
+                    }
+                });
+            } else {
+                //both start and end dates are valid
+                console.log("Start and end dates valid");
+                all_records = await prisma.historical_Stock_Price.findMany({
+                    where:{
+                        stockID : stock_id,
+                        Date : {
+                            gte: hsp_start_date,
+                            lte: hsp_end_date
+                        }
+                    },
+                    orderBy: {
+                        Date : sort
+                    }
+                });
+            }
+
+            successMsg = `Found ${all_records.length} records for ${ticker_symbol}`;
             console.log(successMsg);
             res.status(200).json({
                 "message" : successMsg,
