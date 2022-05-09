d76c66e3a48de6647824518a9563d87d88c2731a
harixcode <hari1000prasad@gmail.com>
2022-05-10 04:10:33 +0800

    minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import Layout from '../components/layout'
+import styles from '../styles/Home.module.css'
 
 export default function Page () {
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
+      <Head>
+        
+      </Head>
+      <div className={styles.main}>
+      <h1 >NextAuth.js Example</h1>
       <p>
         This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
       </p>
+      </div>
     </Layout>
   )
 }
