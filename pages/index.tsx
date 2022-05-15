54a1874b99b1a468f1b5f51f6086d7cb98a30cd8
svenandten <rickezxc@gmail.com>
2022-05-16 01:44:41 +0800

    updated landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
+// pages/index.tsx
+import prisma from '../lib/prisma';
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
+import * as React from "react"
 import Head from 'next/head'
-export default function Page () {
+
+import DataTable from '../components/Table';
+
+export const getServerSideProps = async () => {
+  const res = await fetch("http://localhost:3000/api/stock/get_all_stocks");
+  const stocks = await res.json();
+  return {
+    props : { stocks, length:stocks.result.length},
+    // props : {stocksList:stocks.result}
+  };
+};
+
+export default function Page (props){
   return (
     <Layout>
       <Head>
@@ -13,9 +28,17 @@ export default function Page () {
       <p>
         This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
       </p>
+      <p>
+        {/* {JSON.stringify(props.stocks.result)} */}
+        {JSON.stringify(props.stocks.result)}
+      </p>
+      <DataTable data={props}/>
+
       <a href='/stockpage'>Stockpage Example</a>
       <a href='/testpage'>Test Page</a>
       </div>
     </Layout>
   )
 }
+
+
---
+++ pages/table.tsx
@@ -0,0 +1,34 @@
+import React, {useMemo} from "react"
+import {useTable} from "react-table"
+import {Types} from '../components/types'
+
+type Props ={
+    data: Types[];
+};
+//if its static use this one , if its variable =>useMemo
+// const columns = [
+//     Header:"Id",
+//     accessor:"Id"
+// ];
+
+function Table(props:Props){
+    //catching the data. Purpose is to only have 1 instance
+    // const data = useMemo(()=> props.data, [props.data]);
+    // const {
+    //     getTableProps,
+    //     getTableBodyProps,
+    //     headerGroups,
+    //     rows,
+    //     prepareRow,
+    // } = useTable({columns,data});
+
+    return 
+        // <table>
+        //     <thead>
+        //         {headerGroups.map((headerGroup)=>(
+        //             <tr
+        //         ))}
+        //     </thead>
+        // </table>
+    
+}
