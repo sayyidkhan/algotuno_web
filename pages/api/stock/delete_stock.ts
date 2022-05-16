17b10ed93667b972dc50b0b13c87fb57f347e6b8
svenandten <rickezxc@gmail.com>
2022-05-16 12:52:18 +0800

    updated delete_stock.ts

--- pages/api/stock/delete_stock.ts
+++ pages/api/stock/delete_stock.ts
@@ -4,11 +4,27 @@ export default async (req, res) => {
 
     if (req.method === "POST"){
 
-        let ticker_symbol;
+        let ticker_symbol, stock_id;
 
         try{
             ticker_symbol = req.body.ticker_symbol;
+            
+            const stock_record = await prisma.stock.findFirst({
+                where:{
+                    tickerSymbol : ticker_symbol
+                }
+            })
 
+            if (stock_record) {
+                // return the corresponding stockID
+                stock_id = stock_record.stockID;
+            } else {
+                console.log(`Stock ${ticker_symbol} does not exist`)
+                return res.status(406).json({
+                    "message" : `Stock ${ticker_symbol} does not exist`
+                });
+            }  
+            
             // delete all instances where the ticker_symbol matches
             const delete_stock_result = await prisma.stock.deleteMany({
                 where:{
