08eb147830f86f0d5da925cb3baa501fa3bec979
svenandten <rickezxc@gmail.com>
2022-05-16 13:05:53 +0800

    updated populate_hsp.ts

--- pages/api/stock/populate_hsp.ts
+++ pages/api/stock/populate_hsp.ts
@@ -25,9 +25,16 @@ export default async (req, res) => {
                     tickerSymbol : ticker_symbol
                 }
             })
-
-            // return the corresponding stockID
-            stock_id = stock_record.stockID
+            
+            if (stock_record) {
+                // return the corresponding stockID
+                stock_id = stock_record.stockID;
+            } else {
+                console.log(`Stock ${ticker_symbol} does not exist`)
+                return res.status(406).json({
+                    "message" : `Stock ${ticker_symbol} does not exist`
+                });
+            } 
 
         } catch (error) {
             const errorMsg = error.message;
