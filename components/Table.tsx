c95a3b15740c8c382b2002c1f41b5431f75db859
svenandten <rickezxc@gmail.com>
2022-05-22 17:58:46 +0800

    reworked logic on db urls

--- components/Table.tsx
+++ components/Table.tsx
@@ -9,11 +9,18 @@ interface StockDate {
 }
 
 const columns: GridColDef[] = [
-  { field: 'stockID', headerName: 'stockID', width: 70 },
-  { field: 'tickerSymbol', headerName: 'ticker', width: 130 },
-  { field: 'companyName', headerName: 'company', width: 130 },
-  { field: 'exchange', headerName: 'Exchange', width: 130}
+  { field: 'stockID', headerName: 'Stock ID', width: 90, description: 'Stock ID to uniquely identify the stock.'},
+  { field: 'tickerSymbol', headerName: 'Ticker', width: 90, description:'Ticker symbol to uniqeuly identify the stock'},
+  { field: 'companyName', headerName: 'Company', width: 180, description:'Name of the company'},
+  { field: 'exchange', headerName: 'Exchange', width: 110, description:'Exchange where the stock is being traded'}
 ];
+
+// ,
+//   { field: '', headerName: 'Stock Price', width: 120, description:'Latest Stock Price (EOD)'},
+//   { field: '', headerName: 'Market Cap', width: 120, description:'Latest Market Cap of the company'},
+//   { field: '', headerName: '7D ', width: 120, description:'7 Day Price Change'},
+//   { field: '', headerName: '14D ', width: 120, description:'14 Day Price Change'},
+//   { field: '', headerName: '30D ', width: 120, description:'30 Day Price Change'}
 
 
 export default function DataTable(props) {
@@ -22,15 +29,22 @@ export default function DataTable(props) {
   console.log(JSON.stringify(props));
 
   return (
-    <div style={{ height: 400, width: '80%' }}>
-      <DataGrid
-        getRowId={(rows)=>rows.stockID}
-        rows={sl}
-        columns={columns}
-        pageSize={4}
-        rowsPerPageOptions={[4]}
-        checkboxSelection
-      />
+    // <div style={{ height: 270, width: '70%' }}>
+    <div style ={{display:'flex', width:'70%'}}>
+      <div style={{ flexGrow: 1 }}>
+        <DataGrid 
+          autoHeight
+          getRowId={(rows)=>rows.stockID}
+          rows={sl}
+          columns={columns}
+          pageSize={3}
+          rowsPerPageOptions={[3]}
+          // checkboxSelection
+          onCellClick={(params, event) => {
+            document.location.href = '/trending'
+          }}
+        />
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
 // pages/index.tsx
-import prisma from '../lib/prisma';
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
 import * as React from "react"
 import Head from 'next/head'
+import db_url from '../lib/db_prod_checker';
 
 import DataTable from '../components/Table';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 
 export const getServerSideProps = async () => {
-  const res = await fetch("http://localhost:3000/api/stock/get_all_stocks");
+
+  const res = await fetch(db_url+'/api/stock/get_all_stocks');
   const stocks = await res.json();
   return {
-    props : { stocks, length:stocks.result.length},
-    // props : {stocksList:stocks.result}
+    props : { stocks, length:stocks.result.length}
   };
 };
 
@@ -20,22 +21,22 @@ export default function Page (props){
   return (
     <Layout>
       <Head>
-      <title>Home</title>
+        <title>Home</title>
       </Head>
       
       <div className={styles.main}>
-      <h1 >NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
-      </p>
-      <p>
-        {/* {JSON.stringify(props.stocks.result)} */}
-        {/* {JSON.stringify(props.stocks.result)} */}
-      </p>
-      <DataTable data={props}/>
 
-      <a href='/stockpage'>Stockpage Example</a>
-      <a href='/testpage'>Test Page</a>
+        <h1>Stock Prices by Market Cap 
+          <p className={styles.myh}>
+            <WarningAmberIcon sx={{ fontSize: 10 }}/> Pricing data updated on daily basis. Currency in USD.
+          </p>
+        </h1>
+          
+          
+        <DataTable data={props}/>
+
+        <a href='/stockpage'>Stockpage Example</a>
+        <a href='/testpage'>Test Page</a>
       </div>
     </Layout>
   )
