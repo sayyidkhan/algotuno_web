b57cfd29e6540cd8dd1d87453c720228c039e30d
sayyidkhan <sayyidkhan92@hotmail.com>
2022-05-26 00:47:08 +0800

    hotfix

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
-
+import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 const AboutPage = () => (
   <Layout>
+    <Head>
+      <title>About Us</title>
+    </Head>
+    <div className={styles.main}>
     <h1>About</h1>
     <p>This is the about page</p>
     <p>
@@ -10,6 +15,8 @@ const AboutPage = () => (
         <a>Go home</a>
       </Link>
     </p>
+    </div>
+    
   </Layout>
 )
 
