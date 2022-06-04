cf1589b9b8894f8af239715c5e50ce3a9cc0642c
harixcode <hari1000prasad@gmail.com>
2022-06-05 03:16:02 +0800

    merge dev into hari

--- pages/api/api.tsx
+++ pages/api/api.tsx
@@ -1,4 +1,4 @@
 import {BASE_URL} from "../../lib/db_prod_checker";
-//export const GetAllStocks=()=> ` http://localhost:3000/api/stock/get_all_stocks`;
+export const GetAllStocks=()=> 'http://localhost:3000/api/stock/get_all_stocks';
 //https://algotuno-web3-ovh08k87o-algotuno.vercel.app/
-export const GetAllStocks=()=> `https://algotuno-web3-ovh08k87o-algotuno.vercel.app/api/stock/get_all_stocks`;
+//export const GetAllStocks=()=> `https://algotuno-web3-ovh08k87o-algotuno.vercel.app/api/stock/get_all_stocks`;
