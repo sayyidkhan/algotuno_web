a3632d514bc3ae5011a2113364a2239f2e9926a8
harixcode <hari1000prasad@gmail.com>
2022-06-06 02:57:08 +0800

    fixed base_url in axios

--- components/index/Table.tsx
+++ components/index/Table.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
-import { DataGrid, GridColDef} from '@mui/x-data-grid';
-import {BASE_URL} from '../../lib/db_prod_checker';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 import { makeStyles } from "@mui/styles";
-import axios from 'axios';
 import axios_api from '../../pages/api/axios_api';
 import {
   Container,
@@ -21,75 +18,11 @@ import {
   Paper,
 } from "@mui/material"
 import { useEffect, useState } from 'react';
-import { GetAllStocks } from '../../pages/api/api';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
 
 export let watchliststorage=[];
 
-
-// interface StockDate {
-//   stockID : number,
-//   tickerSymbol  : string,
-//   companyName : string,
-//   exchange : string
-// }
-
-// const columns: GridColDef[] = [
-//   { field: 'stockID', headerName: 'Stock ID', width: 90, description: 'Stock ID to uniquely identify the stock.'},
-//   { field: 'tickerSymbol', headerName: 'Ticker', width: 90, description:'Ticker symbol to uniqeuly identify the stock'},
-//   { field: 'companyName', headerName: 'Company', width: 180, description:'Name of the company'},
-//   { field: 'exchange', headerName: 'Exchange', width: 110, description:'Exchange where the stock is being traded'}
-// ];
-
-// ,
-//   { field: '', headerName: 'Stock Price', width: 120, description:'Latest Stock Price (EOD)'},
-//   { field: '', headerName: 'Market Cap', width: 120, description:'Latest Market Cap of the company'},
-//   { field: '', headerName: '7D ', width: 120, description:'7 Day Price Change'},
-//   { field: '', headerName: '14D ', width: 120, description:'14 Day Price Change'},
-//   { field: '', headerName: '30D ', width: 120, description:'30 Day Price Change'}
-
-
-// export function DataTable(props) {
-
-//   const sl = props.data.stocks.result;
-//   //console.log(JSON.stringify(props));
-
-//   return (
-//     // <div style={{ height: 270, width: '70%' }}>
-//     <div style ={{ width:'90%'}}>
-//       <div style={{ flexGrow: 1 }}>
-//         <DataGrid 
-//           autoHeight
-//           getRowId={(rows)=>rows.stockID}
-//           rows={sl}
-//           columns={columns}
-//           pageSize={3}
-//           rowsPerPageOptions={[3]}
-//           // checkboxSelection
-//           onCellClick={(params) => {
-//             //document.location.href = '/trending';
-//             //console.log(params);
-//             if(params.field === "tickerSymbol")
-//             {
-//               //document.location.href = '/trending';
-//               //console.log(params.row.tickerSymbol);
-//               //tickerName = `${params.row.tickerSymbol}`;
-//               //console.log(tickerName);
-              
-//               const URL = `/stockpage?tickerSymbol=${params.row.tickerSymbol}`;
-//               //<Link href={URL}></Link>;
-              
-//               document.location.href = URL;
-//           //     //console.log(URL);
-//             }
-//           }}    
-          
-//         />
-//       </div>
-//     </div>
-//   );
-// }
 
 export default function StocksList (props){
   const [stocks, setStocks] = useState([]);
