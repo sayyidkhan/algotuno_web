997794a71f7a8b5f3883b32aae2f1a20ee3ac94a
sayyidkhan <sayyidkhan92@hotmail.com>
2022-06-10 00:07:22 +0800

    fix axios issue

--- config/axios_api.ts
+++ config/axios_api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { BASE_URL } from '../../config/db_prod_checker';
+import { BASE_URL } from './db_prod_checker';
 
 
 //change the port the number to be the same backend port number, in order for the api to be able to talk
