9a0a12b33074ed02d69a9137823a230d026bf845
svenandten <rickezxc@gmail.com>
2022-06-28 09:14:57 +0800

    split daily ml price updating into 2 scripts

---
+++ config/ml_endpoints.ts
@@ -0,0 +1,2 @@
+export const TENSORFLOW_URL = "https://algotunotfjsv3.azurewebsites.net/tfjs_run_model/";
+export const SCITKITLEARN_URL = "https://q6p47mowxp5fy2dkeh6cvg6dwi0dykpt.lambda-url.us-east-1.on.aws/";
