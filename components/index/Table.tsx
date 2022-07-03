00b929857b30d60ff7d668b4e9146c97a6bbdce0
sayyidkhan <sayyidkhan92@hotmail.com>
2022-07-03 14:21:14 +0800

    add api in stock management

--- components/index/Table.tsx
+++ components/index/Table.tsx
@@ -57,6 +57,7 @@ export default function StocksList (props){
     setStocks(data.result);
     setLoading(false);
   };
+
   useEffect(() => {
     fetchStocks();
     // eslint-disable-next-line react-hooks/exhaustive-deps
