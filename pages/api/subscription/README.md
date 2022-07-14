a4283b8072338438fcb66d732b4b3cff8e801dfc
svenandten <rickezxc@gmail.com>
2022-07-14 09:24:32 +0800

    added and updated subscription APIs

--- pages/api/subscription/README.md
+++ pages/api/subscription/README.md
@@ -1,13 +1,16 @@
 ## Overview
 Added the following scripts:
-- update_subscription.ts
+- update_user_subscription.ts
+- add_subscription_plan.ts
+- get_all_subscription_plan.ts
+- update_subscription_plan.ts
 
 #### NOTICE
 Some endpoints expect an authorization key in the Headers of each request. The authorization key must be indicated in the Headers as "authorization" : "NEXT_PUBLIC_API_SECRET_KEY **INSERT SECRET KEY**"
 
 #### Usage
-## update_subscription.ts
-To update a user's subscription plan, send a POST request to the **/api/subscription/update_subscription** endpoint with the body contents as such:
+## update_user_subscription.ts
+To update a user's subscription plan, send a POST request to the **/api/subscription/update_user_subscription** endpoint with the body contents as such:
 
 Example:
 
@@ -17,3 +20,33 @@ Example:
     "subscription_plan_id"  : 1 
 }
 ```
+
+## add_subscription_plan.ts
+To add a new subscription plan, send a POST request to the **/api/subscription/add_subscription_plan** endpoint with the body contents as such:
+
+Example:
+```
+{
+    "plan_name" : "some plan name",
+    "price"     : 55
+}
+```
+
+## get_all_subscription_plan.ts
+To get all the subscription plans and the **users_id** (all users) for each subscription plan, send a GET request to the **/api/subscription_get_all_subscription_plan** endpoint.
+
+## update_subscription_plan.ts
+To update the details of a subscription plan, send a POST request to the **/api/subscription/update_subscription_plan** endpoint with the body contents as such:
+
+Example:
+
+```
+{
+    "subscription_plan_id" : 1,
+    "plan_name" : "Flex",
+    "price"     : 30
+}
+```
+
+Notes:
+- To update only the **plan_name**, omit the **price** field and vice versa.
---
+++ pages/api/subscription/update_subscription_plan.ts
@@ -0,0 +1,63 @@
+import prisma from '../../../config/prisma';
+
+export default async (req, res) => {
+
+    if (req.method === "POST"){
+
+        const subscriptionPlanID = req.body.subscription_plan_id;
+        const planName = req.body.plan_name;
+        const price = req.body.price;
+        let data = {};
+
+        if(subscriptionPlanID){
+            try{
+                if (planName || price){
+                    if(planName){
+                        data["planName"] = planName;
+                    }
+
+                    if(price){
+                        data["price"] = price;
+                    }
+
+                    const update_subscription_plan = await prisma.subscription_Plan.update({
+                        where : {
+                            subscriptionPlanID : subscriptionPlanID
+                        }, 
+                        data: data
+                    })
+
+                    res.status(200).json({
+                        "message" : `Successfully updated subscription plan ${subscriptionPlanID}'s details`,
+                        "result" : update_subscription_plan
+                    });
+
+                } else {
+                    res.status(406).json({
+                        "message" : "Specify the plan_name OR price."
+                    });
+                        
+                }
+            } catch (error){
+                const code = error.code;
+                console.error(error.message)
+                // error code for record not found
+                if (code === 'P2025'){
+                    res.status(406).json({"message":`Failed to update subscription plan; record not found`});
+                } else {
+                    res.status(406).json({"message" : error.message});
+                }
+            }
+        } else{
+            res.status(406).json({
+                "message" : "Specify the subscription_plan_id"
+            });
+        }
+
+       
+    } else {
+        res.status(406).json({"message": `ERROR: ${req.method} method used; this endpoint only accepts POST methods`});
+    }
+    
+
+}
