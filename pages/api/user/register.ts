f9a5488e383cf5add1b41dac8a55561757689e59
svenandten <rickezxc@gmail.com>
2022-07-15 18:03:57 +0800

    updated register workflow to add new user to tier 1 subscription plan

--- pages/api/user/register.ts
+++ pages/api/user/register.ts
@@ -1,5 +1,5 @@
 import { registerBasicUser } from "../auth/[...nextauth]";
-
+import {BASE_URL} from '../../../config/db_prod_checker';
 
 export default async (req, res) => {    
     let body = {};
@@ -16,6 +16,25 @@ export default async (req, res) => {
         
         try {
             const result = await registerBasicUser({ email, username, password });
+            const new_user_id = result.id;
+            // console.log(`new user id is ${new_user_id}`)
+
+            // add user to free subscription plan
+            const update_subscription = await fetch(BASE_URL+`/api/subscription/update_user_subscription`, {
+                method : 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  "user_id" : new_user_id,
+                  "subscription_plan_id" : 1
+                })
+            });
+
+            const subs_result = await update_subscription.json();
+            console.log(subs_result)
+            
             const successMsg = `Username ${username} Created Successfully`;
             console.log(successMsg);
             res.status(200).json({ "message" : successMsg });
