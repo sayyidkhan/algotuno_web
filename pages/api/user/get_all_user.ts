c8721e360b50a898807ffa4dccf6bc1dc10a158a
svenandten <rickezxc@gmail.com>
2022-07-17 13:20:16 +0800

    get_all_user API returns subscription, watchlist, settings info

--- pages/api/user/get_all_user.ts
+++ pages/api/user/get_all_user.ts
@@ -2,50 +2,48 @@ import prisma from '../../../config/prisma';
 
 export default async (req, res) => {
 
-    if (req.method === "GET"){
+    if (req.method === "GET") {
 
-        try{
+        try {
             const all_users = await prisma.user.findMany({
-                include:{
-                    Superuser:{
-                        select:{
+                select: {
+                    id: true,
+                    name: true,
+                    email: true,
+                    username: true,
+                    emailVerified: true,
+                    Superuser: {
+                        select: {
                             superuserID: true
                         }
-                    }
+                    },
+                    Subscription: {
+                        select: {
+                            subscriptionID: true,
+                            Subscription_Plan: true
+                        }
+                    },
+                    Stock_Watchlist: true,
+                    User_Settings: true
                 }
-            });
-
-            try {
-                all_users.forEach(e=>{
-                    if (e.Superuser.length == 1){
-                        e["superuserID"]= e.Superuser[0]["superuserID"];
-                    }
-                    
-                    delete e.Superuser;
-                    delete e.password;
-                    delete e.image;
-
-                })
-            }catch(error){
-
-            }
+            })
 
             const successMsg = `Found ${all_users.length} users`;
             console.log(successMsg);
             res.status(200).json({
-              "message" : successMsg,
-              "result"  : all_users 
+                "message": successMsg,
+                "result": all_users
             });
 
         } catch (error) {
             const errorMsg = error.message;
             console.error(errorMsg)
-            res.status(406).json({"message" : errorMsg});
+            res.status(406).json({ "message": errorMsg });
         }
-       
+
     } else {
-        res.status(406).json({"message": `ERROR: ${req.method} method used; this endpoint only accepts GET methods`});
+        res.status(406).json({ "message": `ERROR: ${req.method} method used; this endpoint only accepts GET methods` });
     }
-    
+
 
 }
