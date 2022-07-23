c6d910f888bd3d0bc77824cc8db89a392792d051
svenandten <rickezxc@gmail.com>
2022-07-23 14:07:04 +0800

    changed add_stock.ts to only require ticker symbol

--- pages/api/stock/add_stock.ts
+++ pages/api/stock/add_stock.ts
@@ -4,37 +4,51 @@ export default async (req, res) => {
 
     if (req.method === "POST"){
 
-        let query;
+        const ticker_symbol = req.body.ticker_symbol;
 
-        // check if ticker_symbol, company_name and exchange name is specified
-        try{
-            query = {
-                "tickerSymbol"  : req.body.ticker_symbol,
-                "companyName"   : req.body.company_name, 
-                "exchange"      : req.body.exchange
+        if(ticker_symbol){
+
+            const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${ticker_symbol}&lang=en-US&fields=longName`;
+
+            console.log(`Fetching stock details from ${url}`)
+            const req_stock_details = await fetch(url);
+            const stock_details = await req_stock_details.json();
+
+            if (stock_details["quoteResponse"]["result"].length > 0){
+
+                const query = {
+                    "tickerSymbol"  : ticker_symbol,
+                    "companyName"   : stock_details["quoteResponse"]["result"][0]["longName"], 
+                    "exchange"      : stock_details["quoteResponse"]["result"][0]["fullExchangeName"]
+                }
+
+                try {
+                    const add_stock_result = await prisma.stock.create({data:query});
+                    const successMsg = `Inserted stock ${ticker_symbol}`;
+                    console.log(successMsg);
+                    res.status(200).json({
+                        "message"   : successMsg,
+                        "result"    : add_stock_result
+                    });
+                } catch (error) {
+                    const errorMsg = error.message;
+                    console.error(errorMsg)
+                    res.status(406).json({"message" : errorMsg});
+                }
+  
+            } else {
+                console.log("Ticker Symbol not found in Yahoo Finance API.")
+                res.status(406).json({
+                    "message" : "Ticker Symbol not found in Yahoo Finance API.",
+                    "result"  : ""
+                })
             }
-        } catch (error) {
-            const exceptionMsg = error.message;
-            console.error(exceptionMsg)
+
+        } else{
+            console.log(`Specify ticker_symbol`)
             res.status(406).json({
-                "message" : "Specify the ticker_symbol, company_name and exchange_name",
-                "exception" : exceptionMsg
+                "message": `Specify ticker_symbol`
             });
-        }
-
-        try{
-            const add_stock_result = await prisma.stock.create({data:query});
-            const successMsg = `Inserted stock ${req.body.ticker_symbol}, ${req.body.company_name}, ${req.body.exchange}`;
-            console.log(successMsg);
-            res.status(200).json({
-                "message" : successMsg,
-                "result" : add_stock_result
-            });
-
-        } catch (error) {
-            const errorMsg = error.message;
-            console.error(errorMsg)
-            res.status(406).json({"message" : errorMsg});
         }
        
     } else {
