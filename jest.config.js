bba5232bcd279ab0f8855faddb9a2ec3f677ada9
sayyidkhan <sayyidkhan92@hotmail.com>
2022-07-24 19:54:39 +0800

    add boilerplate code for the testing of the api endpoint

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+// const nextJest = require("next/jest");
+// const createJestConfig = nextJest({
+//     dir: "./",
+// });
+// const customJestConfig = {
+//     moduleDirectories: ["node_modules", "<rootDir>/"],
+//     testEnvironment: "jest-environment-jsdom",
+// };
+// module.exports = createJestConfig(customJestConfig);
+
+module.exports = {
+    clearMocks: true,
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    setupFilesAfterEnv: ['./singleton.ts'],
+}
